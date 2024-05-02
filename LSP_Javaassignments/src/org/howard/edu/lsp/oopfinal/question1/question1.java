e8712ee6a1c8dcc9bdaabf38f7098cd997ede8aa
Chrislegend27 <batsonfocus@gmail.com>
2024-05-02 17:17:25 -0400

    Question 1 init

---
+++ LSP_Javaassignments/src/org/howard/edu/lsp/oopfinal/question1/question1.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.oopfinal.question1;
+
+public class question1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world");
+	}
+
+}
